ff5d7a804af8a0c903c3f442acf91d972c6533e0
XYIAN <kylexyiandilbeck@gmail.com>
2025-04-28 22:03:26 -0700

    team list imag fix

--- src/constants/TEAM_LIST.ts
+++ src/constants/TEAM_LIST.ts
@@ -2,14 +2,6 @@ import { MemberDetail } from '@/types';
 
 export const TEAM_LIST: MemberDetail[] = [
     {
-        name: 'Kyle Dilbeck',
-        id: 1,
-        imageSrc:
-            'https://github.com/XYIAN/headshots/blob/main/headshot%20-%208.png?raw=true',
-        position: 'Software Development',
-        bio: 'I write code, squash bugs, and make sure everything runs smoother than your morning cold brew. Whether it’s scaling apps, optimizing performance, or leading teams, I’m here to build software that just works—so you can focus on the big picture.',
-    },
-    {
         name: 'Rogelio Rodriguez',
         id: 2,
         imageSrc:
@@ -18,10 +10,19 @@ export const TEAM_LIST: MemberDetail[] = [
         bio: 'Whether it’s front-end magic, back-end finesse, or just making sure your app doesn’t crash, I’ve got it covered.',
     },
     {
+        name: 'Kyle Dilbeck',
+        id: 1,
+        imageSrc:
+            'https://github.com/XYIAN/headshots/blob/main/headshot%20-%208.png?raw=true',
+        position: 'Software Development',
+        bio: 'I write code, squash bugs, and make sure everything runs smoother than your morning cold brew. Whether it’s scaling apps, optimizing performance, or leading teams, I’m here to build software that just works—so you can focus on the big picture.',
+    },
+
+    {
         name: 'Gabriel Villadpudua',
         id: 4,
         imageSrc:
-            'https://media.licdn.com/dms/image/v2/D5603AQEUl7SOaeWDYg/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1725493937498?e=1745452800&v=beta&t=44IME760pVDNY3d8OlsyCMAgrCr2nj-pQ9ypvxSpIbQ',
+            'https://github.com/XYIANSoftware/images/blob/main/projects/coming%20soon%20square.png?raw=true',
         position: 'Sales & Marketing',
         bio: 'I sell, market, and make sure people actually know how awesome our products are. If there’s a strategy to scale, a campaign to launch, or a way to make your brand stand out, I’m on it. Also, probably the only person here who owns more than five dress shirts.',
     },
@@ -29,7 +30,7 @@ export const TEAM_LIST: MemberDetail[] = [
         name: 'Joel Sandoval',
         id: 3,
         imageSrc:
-            'https://media.licdn.com/dms/image/v2/C5603AQEBK58WD5IfhQ/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1516777231990?e=1745452800&v=beta&t=GTzkWRwFTCbN8T9itgILdkC63GcpA7FHh3uPUIla-hU',
+            'https://github.com/XYIANSoftware/images/blob/main/projects/coming%20soon%20square.png?raw=true',
         position: 'Sales & Marketing',
         bio: 'I make sure the right people see the right product at the right time—and actually want to buy it. Sales and marketing isn’t just about closing deals, it’s about making connections, building trust, and having just the right amount of hustle.',
     },
